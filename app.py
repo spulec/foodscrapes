03348064760539642acfe17d638c0bd97ecb6c72
Steve Pulec <spulec@gmail.com>
2019-10-09 09:51:06 -0500

    Use chalice

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import os
 import time
 
 import boto3
+from chalice import Chalice, Cron
 from github import Github
 from lxml import html
+from pytz import timezone
+import pytz
 import requests
 
 s3_client = boto3.client('s3')
@@ -13,7 +16,11 @@ textract_client = boto3.client('textract')
 BUCKET_NAME = 'foodscrapes'
 REPO_NAME = 'spulec/foodscrapes'
 
-def main():
+app = Chalice(app_name="foodscrapes")
+
+
+@app.schedule(Cron(0, 20, '*', '*', '?', '*'))
+def main(event):
     res = requests.get("https://www.thomaskeller.com/tfl/menu")
     root = html.fromstring(res.content)
     menu_link = root.xpath("//a[contains(@type, 'pdf')]")[0].attrib['href']
@@ -21,6 +28,12 @@ def main():
     menu_res = requests.get(menu_link)
     s3_client.put_object(
         Body=menu_res.content,
+        Bucket=BUCKET_NAME,
+        Key=file_name,
+    )
+
+    # For chalice policy autogen
+    s3_client.get_object(
         Bucket=BUCKET_NAME,
         Key=file_name,
     )
@@ -46,7 +59,8 @@ def main():
         if 'Text' in block and block['BlockType'] == 'LINE'
     ]
 
-    today = datetime.date.today()
+    date = datetime.datetime.today()
+    today = date.astimezone(timezone('US/Pacific')).date()
     date_string = f"{today.year}-{today.month}-{today.day}"
     out_file = f"{date_string}.txt"
 
